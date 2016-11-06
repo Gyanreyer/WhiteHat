a22a24e882aedda4d02903511d26ce6723b5b4f7
rwg9692 <rwg9692@rit.edu>
2016-11-06 18:55:21 -0500

    Quick push, sprites for player added

--- WhiteHat/Assets/Scripts/Player.cs
+++ WhiteHat/Assets/Scripts/Player.cs
@@ -3,24 +3,40 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    enum PlayerState
+    {
+        idle,
+        running,
+        dead
+    }
+
     public float moveSpeed = 5;
     private Camera mainCamera;
     private Rigidbody2D rigidBody;
+
+    private GameObject legs;
 
     private Vector2 velocity;
 
     private float lerpTime = 0;
 
+    private PlayerState state;
+
 	// Use this for initialization
 	void Start () {
         mainCamera = Camera.main;
         rigidBody = GetComponent<Rigidbody2D>();
+
+        legs = GameObject.Find("Legs");
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //Move the player with WASD, sets velocity to apply to rigidbody in FixedUpdate
         velocity = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")).normalized*moveSpeed;
+
+        legs.transform.eulerAngles = new Vector3(0,0,Mathf.Atan2(velocity.y, velocity.x)*Mathf.Rad2Deg - 90);
+        legs.transform.position = transform.position + new Vector3(0,0,1);
 
         if(velocity == Vector2.zero)
         {
